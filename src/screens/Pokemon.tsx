839614989af0bb474f867969e4c32c1f792561bf
VictorRF <victor_rofh@hotmail.com>
2023-04-13 17:36:55 -0600

    Tercer juego: adivinar pokemon

--- src/screens/Pokemon.tsx
+++ src/screens/Pokemon.tsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import { useState } from "react";
+
+const POKEMONS = [
+  "Bulbasaur",
+  "Ivysaur",
+  "Venusaur",
+  "Charmander",
+  "Charmeleon",
+  "Charizard",
+  "Squirtle",
+  "Wartortle",
+  "Blastoise",
+  "Caterpie",
+  "Metapod",
+  "Butterfree",
+  "Weedle",
+  "Kakuna",
+  "Beedrill",
+  "Pidgey",
+  "Pidgeotto",
+  "Pidgeot",
+  "Rattata",
+  "Raticate" ].map((p) => p.toLowerCase());
+
+  const MATCH = Math.floor(Math.random() * POKEMONS.length);
+
+  type Form = HTMLFormElement & {
+    pokemon: HTMLInputElement;
+  }
 
 export const Pokemon = () => {
+  const [hasWon, sethasWon] = useState(false);
+
+  function handleSubmit(event: React.FormEvent<Form>) {
+    event.preventDefault();
+    const {pokemon} = event.currentTarget;
+    if ( pokemon.value.toLowerCase() === POKEMONS[MATCH]) {
+      sethasWon(true);
+      alert("You won!");
+    } else {
+      alert("Wrong answer!");
+    }
+  }
+
   return (
-    <div>Pokemon</div>
+    <div>
+      <img
+      height={512}
+      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${MATCH + 1}.png`} alt="pokemon sprite"
+      style={{imageRendering: "pixelated", filter: hasWon ? "" : "brightness(0) invert(1)"}}
+      width={512}/>
+      { hasWon ? (
+        <button style={{width: "100%"}} onClick={() => location.reload()}>
+          Play again
+        </button>
+        ) : (
+          <form onSubmit={handleSubmit}>
+          <input name="pokemon" type="text" />
+          <button type="submit">Submit</button>
+        </form>
+        )
+      }
+    </div>
   )
 }
